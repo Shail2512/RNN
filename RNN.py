2938fcc03d3ba3d7d63ce743b744e32a3f0ec642
Shail2512 <raval.shail1@gmail.com>
2021-10-31 10:36:23 -0400

    Minor Changes

--- RNN.py
+++ RNN.py
@@ -1,6 +1,5 @@
 import torch
-from torch import nn
-class RNNModel(nn.Module):
+class RNNModel(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
         super(RNNModel, self).__init__()
 
@@ -9,16 +8,16 @@ class RNNModel(nn.Module):
         self.layer_dim = layer_dim
 
         # RNN layers
-        self.rnn = nn.RNN(input_dim, hidden_dim, layer_dim)
+        self.rnn = torch.nn.RNN(input_dim, hidden_dim, layer_dim)
         # Fully connected layer
-        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.fc = torch.nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
         # Initializing hidden state for first input with zeros
         hidden_state_0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
 
         # Forward propagation by passing in the input and hidden state into the model
-        out, hidden_state_0 = self.rnn(x, hidden_state_0.detach())
+        out, hidden_state_0 = self.rnn(x, hidden_state_0)
 
         # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
         # so that it can fit into the fully connected layer
