290dbdd77d17ad5f4436dd1b03f7ec999a948a45
Shail2512 <raval.shail1@gmail.com>
2021-10-31 10:30:18 -0400

    Initial architecture of RNN model

---
+++ RNN.py
@@ -0,0 +1,29 @@
+import torch
+from torch import nn
+class RNNModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(RNNModel, self).__init__()
+
+        # Defining the number of layers and the nodes in each layer
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+
+        # RNN layers
+        self.rnn = nn.RNN(input_dim, hidden_dim, layer_dim)
+        # Fully connected layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initializing hidden state for first input with zeros
+        hidden_state_0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # Forward propagation by passing in the input and hidden state into the model
+        out, hidden_state_0 = self.rnn(x, hidden_state_0.detach())
+
+        # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
+        # so that it can fit into the fully connected layer
+        out = out[:, -1, :]
+
+        # Convert the final state to our desired output shape (batch_size, output_dim)
+        out = self.fc(out)
+        return out
