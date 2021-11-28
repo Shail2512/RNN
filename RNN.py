57f40f8d1a171c5113b0d6c88fc9ea020fcf15d9
Shail2512 <raval.shail1@gmail.com>
2021-11-27 22:18:38 -0500

    Custom sequential RNN architecture for individual waves

--- RNN.py
+++ RNN.py
@@ -1,28 +1,33 @@
 import torch
 class RNNModel(torch.nn.Module):
-    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+    def __init__(self, feature_dim, hidden_dim, batch_size):
         super(RNNModel, self).__init__()
+        
+        # RNN Architecture
+        self.hidden_size=hidden_dim
+        self.input_size=feature_dim  
+        self.batch_size=batch_size
+        self.num_layers=2
+        
+        # RNN 
+        self.rnn = torch.nn.RNN(feature_dim, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=True)
+        
+        # fc layers
+        self.fc6 = torch.nn.Linear(hidden_dim, 2)
+        self.fc9 = torch.nn.Linear(hidden_dim, 2)
+        self.fc12 = torch.nn.Linear(hidden_dim, 2)
+        self.fc15 = torch.nn.Linear(hidden_dim, 2)    
+        self.linear_list = [self.fc6,self.fc9,self.fc12,self.fc15]
 
-        # Defining the number of layers and the nodes in each layer
-        self.hidden_dim = hidden_dim
-        self.layer_dim = layer_dim
-
-        # RNN layers
-        self.rnn = torch.nn.RNN(input_dim, hidden_dim, layer_dim)
-        # Fully connected layer
-        self.fc = torch.nn.Linear(hidden_dim, output_dim)
-
-    def forward(self, x):
-        # Initializing hidden state for first input with zeros
-        hidden_state_0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
-
-        # Forward propagation by passing in the input and hidden state into the model
-        out, hidden_state_0 = self.rnn(x, hidden_state_0)
-
-        # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
-        # so that it can fit into the fully connected layer
-        out = out[:, -1, :]
-
-        # Convert the final state to our desired output shape (batch_size, output_dim)
-        out = self.fc(out)
-        return out
+    def forward(self, inp):
+        output_list = []
+        # initialize hidden and cell
+        hn = torch.nn.init(torch.randn(self.num_layers, self.batch_size, self.hidden_size))
+        cn = torch.nn.init(torch.randn(self.num_layers, self.batch_size, self.hidden_size))
+        
+        # step through the sequence i.e. for each wave
+        for i in range(len(inp)):
+            output, (hn,cn) = self.rnn(inp[i], (hn,cn))
+            # output is [batch size, timestep = 1, hidden dim]
+            output_list.append(self.linear_list[i](output[:,0,:]))
+        return output_list
