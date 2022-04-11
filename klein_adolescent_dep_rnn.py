d270e12408e0d0f6a6e84bd8612b8422ac4c13ca
Shail Raval <raval.shail1@gmail.com>
2022-04-10 21:20:45 -0400

    Updated Code with final baseline

--- klein_adolescent_dep_rnn.py
+++ klein_adolescent_dep_rnn.py
@@ -2,14 +2,16 @@ from dlatk.featureGetter import FeatureGetter
 import numpy as np
 import torch
 import rnn_model
+import torch.nn.functional as F
+from sklearn import metrics
 fg = FeatureGetter.fromFile('./test.ini')
 data = fg.getGroupNorms()
 np_data = np.array(data)
-print(np_data.shape)
-print(np_data[:652])
+#print(np_data.shape)
+#print(np_data[:652])
 data_age_3_unique = ["CBQ_EC_Mother_age3","AnyDepressionDuke_PAPA_age3","DSMAnxDxDuke_PAPA_age3","ODDDxDuke2_PAPA_age3","ADHDdx_PAPA_age3","CBCL_DSMAffectiveProblems_Mother_age3","CBCL_DSMAnxietyProblems_Mother_age3","CBCL_DSMAttentionDeficitHyperactivityProblems_Mother_age3","CBCL_DSMOppositionalDefiantProblems_Mother_age3","Incapacity_MO_PAPA_age3"]
 data_age_3_consistent = ["CBQ_NA_Mother_age3","CBQ_ApproachAnticipation_Mother_age3","CBQ_SmilingLaughter_Mother_age3","totalstress.typecount.age3","PSDQ_Factor1_Authoritative_Parent_age3","PSDQ_Factor2_Authoritarian_Parent_age3","PSDQ_Factor3_Permissive_Parent_age3","DyadicAdjust_Parent_Abrev_age3"]
-ata_age_6_unique = ["CBQ_EC_Mother_age6","LifeRIFT_total_S_age6"]
+data_age_6_unique = ["CBQ_EC_Mother_age6","LifeRIFT_total_S_age6"]
 data_age_6_consistent = ["CBQ_NA_Mother_age6","CBQ_Age6_ApproachAnticipation_Mother","CBQ_Age6_SmilingLaughter_Mother","totalstress.typecount.age6","PSDQ_Age6_Factor1_Authoritative_Parent","PSDQ_Age6_Factor2_Authoritarian_Parent","PSDQ_Age6_Factor3_Permissive_Parent","DyadicAdjust_Age6_Parent_Abrev"]
 data_age_9_unique = ["AFARS_NAscale_Child_T3","APP_Total_Child_T3","PDSbothgenders_age9_Mother","LifeRIFT_total_S_age9"]
 data_age_9_consistent = ["Eisenberg_ChildReactions_NegativeEmotionsScale_Mother_T3","Eisenberg_ChildReactions_PositiveEmotionsScale_Mother_T3","AFARS_PAscale_Child_T3","Total_Stressors_Trauma_Sum_KSADS9","Acceptance_Rejection_CRPBIc_Parent_age9","Control_Autonomy_CRPBIc_Parent_age9","Firm_LaxControl_CRPBIc_Parent_age9","DAS_Total_age9_Parent"]
@@ -88,27 +90,116 @@ print(final_data_age12_consistent[:5])
 with open("data_age_12_rnn.npy","wb") as f:
     np.save(f, final_data_age12_consistent)
 
+
+
+for i in range(651):
+    group_id.append(data[i][0])
+np_data_age3_unique = []
+for i in group_id:
+    l=[]
+    for j in data_age_3_unique:
+        for k in np_data:
+            if str(k[0]) == str(i) and str(k[1]) == str(j):
+                l.append(k[2])
+    np_data_age3_unique.append(np.array(l))
+final_data_age3_unique = np.array(np_data_age3_unique)
+print("Unique age 3 : ", final_data_age3_unique.shape)
+#print(final_data_age3_unique[:5])
+with open("data_age_3_rnn_unique.npy","wb") as f:
+    np.save(f, final_data_age3_unique)
+
+#for i in range(651):
+#    group_id.append(data[i][0])
+np_data_age6_unique = []
+for i in group_id:
+    l=[]
+    for j in data_age_6_unique:
+        for k in np_data:
+            if str(k[0]) == str(i) and str(k[1]) == str(j):
+                l.append(k[2])
+    np_data_age6_unique.append(np.array(l))
+final_data_age6_unique = np.array(np_data_age6_unique)
+print("Unique age 6 : ", final_data_age6_unique.shape)
+#print(final_data_age6_consistent[:5])
+with open("data_age_6_rnn_unique.npy","wb") as f:
+    np.save(f, final_data_age6_unique)
+
+#for i in range(651):
+#    group_id.append(data[i][0])
+np_data_age9_unique = []
+for i in group_id:
+    l=[]
+    for j in data_age_9_unique:
+        for k in np_data:
+            if str(k[0]) == str(i) and str(k[1]) == str(j):
+                l.append(k[2])
+                break
+    np_data_age9_unique.append(np.array(l))
+final_data_age9_unique = np.array(np_data_age9_unique)
+print("Unique age 9: ", final_data_age9_unique.shape)
+#print(final_data_age9_unique[:5])
+with open("data_age_9_rnn_unique.npy","wb") as f:
+    np.save(f, final_data_age9_unique)
+
+#for i in range(651):
+#    group_id.append(data[i][0])
+np_data_age12_unique = []
+for i in group_id:
+    l=[]
+    for j in data_age_12_unique:
+        for k in np_data:
+            if str(k[0]) == str(i) and str(k[1]) == str(j):
+                l.append(k[2])
+                break
+    np_data_age12_unique.append(np.array(l))
+final_data_age12_unique = np.array(np_data_age12_unique)
+print("Unique age 12: ", final_data_age12_unique.shape)
+#print(final_data_age12_unique[:5])
+with open("data_age_12_rnn_unique.npy","wb") as f:
+    np.save(f, final_data_age12_unique)
+
 '''
 with open("data_age_3_rnn.npy","rb") as f:
     data_age_3_rnn = np.load(f)
 data_age_3_rnn = np.asfarray(data_age_3_rnn)
 data_age_3_rnn = np.reshape(data_age_3_rnn, (1, data_age_3_rnn.shape[0], data_age_3_rnn.shape[1]))
-print(data_age_3_rnn.shape)
+#print(data_age_3_rnn.shape)
 with open("data_age_6_rnn.npy","rb") as f:
     data_age_6_rnn = np.load(f)
 data_age_6_rnn = np.asfarray(data_age_6_rnn)
 data_age_6_rnn = np.reshape(data_age_6_rnn, (1, data_age_6_rnn.shape[0], data_age_6_rnn.shape[1]))
-print(data_age_6_rnn.shape)
+#print(data_age_6_rnn.shape)
 with open("data_age_9_rnn.npy","rb") as f:
     data_age_9_rnn = np.load(f)
 data_age_9_rnn = np.asfarray(data_age_9_rnn)
 data_age_9_rnn = np.reshape(data_age_9_rnn, (1, data_age_9_rnn.shape[0], data_age_9_rnn.shape[1]))
-print(data_age_9_rnn.shape)
+#print(data_age_9_rnn.shape)
 with open("data_age_12_rnn.npy","rb") as f:
     data_age_12_rnn = np.load(f)
 data_age_12_rnn = np.asfarray(data_age_12_rnn)
 data_age_12_rnn = np.reshape(data_age_12_rnn, (1, data_age_12_rnn.shape[0], data_age_12_rnn.shape[1]))
-print(data_age_12_rnn.shape)
+#print(data_age_12_rnn.shape)
+with open("data_age_3_rnn_unique.npy","rb") as f:
+    data_age_3_rnn_unique = np.load(f)
+data_age_3_rnn_unique = np.asfarray(data_age_3_rnn_unique)
+data_age_3_rnn_unique = np.reshape(data_age_3_rnn_unique, (1, data_age_3_rnn_unique.shape[0], data_age_3_rnn_unique.shape[1]))
+print("Age 3 unique : ", data_age_3_rnn_unique.shape)
+with open("data_age_6_rnn_unique.npy","rb") as f:
+    data_age_6_rnn_unique = np.load(f)
+data_age_6_rnn_unique = np.asfarray(data_age_6_rnn_unique)
+data_age_6_rnn_unique = np.reshape(data_age_6_rnn_unique, (1, data_age_6_rnn_unique.shape[0], data_age_6_rnn_unique.shape[1]))
+print(data_age_6_rnn_unique.shape)
+with open("data_age_9_rnn_unique.npy","rb") as f:
+    data_age_9_rnn_unique = np.load(f)
+data_age_9_rnn_unique = np.asfarray(data_age_9_rnn_unique)
+data_age_9_rnn_unique = np.reshape(data_age_9_rnn_unique, (1, data_age_9_rnn_unique.shape[0], data_age_9_rnn_unique.shape[1]))
+print(data_age_9_rnn_unique.shape)
+with open("data_age_12_rnn_unique.npy","rb") as f:
+    data_age_12_rnn_unique = np.load(f)
+data_age_12_rnn_unique = np.asfarray(data_age_12_rnn_unique)
+data_age_12_rnn_unique = np.reshape(data_age_12_rnn_unique, (1, data_age_12_rnn_unique.shape[0], data_age_12_rnn_unique.shape[1]))
+print(data_age_12_rnn_unique.shape)
+
 output_age_3 = ["AnyDepression_T2_Imp"]
 
 for i in range(651):
@@ -151,8 +242,19 @@ for i in group_id:
                 l = float(k[2])
     np_data_age12_output.append(l)
 final_data_age12_output = np.array(np_data_age12_output)
+print("Data age 9 output_data : ", final_data_age9_output[:20])
 #final_data_age6_output = np.reshape(final_data_age6_output, (-1,1))
-#is_available() checks and returns a Boolean True if a GPU is available, else it'll return False
+'''
+print(final_data_age3_output.shape)
+print(final_data_age3_output[:5])
+print(final_data_age6_output.shape)
+print(final_data_age6_output[:5])
+print(final_data_age9_output.shape)
+print(final_data_age9_output[:5])
+print(final_data_age12_output.shape)
+print(final_data_age12_output[:5])
+'''
+#s_available() checks and returns a Boolean True if a GPU is available, else it'll return False
 is_cuda = torch.cuda.is_available()
 
 # If we have a GPU available, we'll set our device to GPU. We'll use this device variable later in our code.
@@ -168,25 +270,39 @@ input_data.append(torch.from_numpy(data_age_6_rnn))
 input_data.append(torch.from_numpy(data_age_9_rnn))
 input_data.append(torch.from_numpy(data_age_12_rnn))
 
+input_data_age3_unique = torch.from_numpy(data_age_3_rnn_unique)
+input_data_age6_unique = torch.from_numpy(data_age_6_rnn_unique)
+input_data_age9_unique = torch.from_numpy(data_age_9_rnn_unique)
+input_data_age12_unique = torch.from_numpy(data_age_12_rnn_unique)
+
+#print("Unique Input Shape : ", X_Unique.shape)
 X = torch.Tensor(651,4,15)
 torch.cat(input_data, out=X.double())
 L = [final_data_age3_output, final_data_age6_output, final_data_age9_output, final_data_age12_output]
 output_data = np.vstack(L)
-print("Output data: ", output_data.shape)
+
+#print("Output data: ", output_data.shape)
 Y = torch.from_numpy(output_data)
-Y = Y.permute((1,0)) 
-
-print(X.size(), " " , Y.size())
-
-
-X_train = X[:585,:,:]
-X_test = X[-66:,:,:]
+Y = Y.permute((1,0))
+Y = torch.round(Y)
+Y_mean = torch.mean(Y, dim=1)
+Y_mean = Y_mean.reshape([Y_mean.shape[0],1])
+input_data_extend = torch.cat((Y_mean, Y[:,0].reshape([Y.shape[0],1]), Y[:,1].reshape([Y.shape[0],1]), Y[:,2].reshape([Y.shape[0],1])), 1)
+input_data_extend = input_data_extend.reshape([input_data_extend.shape[0],input_data_extend.shape[1],1])
+X_extend = torch.cat((X.float(),input_data_extend.float()),2)
+X_train = X_extend[:585,:,:]
+X_test = X_extend[-66:,:,:]
+X_Unique_age3_train = input_data_age3_unique.flatten(0,1)[:585,:]
+X_Unique_age3_test = input_data_age3_unique.flatten(0,1)[-66:,:]
+X_Unique_age6_train = input_data_age6_unique.flatten(0,1)[:585,:]
+X_Unique_age6_test = input_data_age6_unique.flatten(0,1)[-66:,:]
+X_Unique_age9_train = input_data_age9_unique.flatten(0,1)[:585,:]
+X_Unique_age9_test = input_data_age9_unique.flatten(0,1)[-66:,:]
+X_Unique_age12_train = input_data_age12_unique.flatten(0,1)[:585,:]
+X_Unique_age12_test = input_data_age12_unique.flatten(0,1)[-66:,:]
 Y_train = Y[:585,:]
 Y_test = Y[-66:, :]
-#train_set_input, val_set_input = torch.utils.data.random_split(input_data_age6_rnn, [585, 66])
-#train_set_target, val_set_target = torch.utils.data.random_split(target_output_age6, [585, 66])
-print("Train Shape : ", X_train.shape, "Output_shape : ", Y_train.shape)
-model = rnn_model.RNNModel(X_train.shape[0], X_train.shape[1], 128, 4, 2, 1)
+model = rnn_model.RNNModel(X_train.shape[2], X_train.shape[1],128, 4, 2, X_train.shape[0])
 model = model.to(device)
 print(model)
 num_epochs = 100
@@ -199,16 +315,25 @@ optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 for epoch in range(1, num_epochs + 1):
     optimizer.zero_grad() # Clears existing gradients from previous epoch
     #input_seq = input_seq.to(device)
-    output, hidden = model(X_train.float().cuda())
+    #output, hidden = model(X_train.float().cuda(), X_Unique_age3_train.float().cuda(), X_Unique_age6_train.float().cuda(), X_Unique_age9_train.float().cuda(), X_Unique_age12_train.float().cuda())
+    output, hidden = model(X_train.float().cuda(), X_Unique_age3_train.float().cuda(), X_Unique_age6_train.float().cuda(), X_Unique_age9_train.float().cuda())
     output = output.to(device)
-    output = output.reshape(output.shape[0])
+    #output = output.permute((1,0))
+    #output = output.reshape(output.shape[0])
     target_seq = Y_train.to(device)
-    loss = criterion(output, target_seq)
+    target_seq = target_seq.permute((1,0))
+    target_seq = target_seq[:3,:]
+    print("Target Sequence : ", target_seq.shape)
+    target_array = target_seq.cpu().detach().numpy()
+    output_array = output.cpu().detach().numpy()
+    print("Target Array : ",len(target_array[2]), " Output array : ", len(output_array[2]))
+    fpr, tpr, thresholds = metrics.roc_curve(target_array[2], output_array[2])
+    print("AUC :", metrics.auc(fpr, tpr))
+    loss = criterion(output, target_seq.float())
     loss.backward() # Does backpropagation and calculates gradients
     optimizer.step() # Updates the weights accordingly
     
     if epoch%5 == 0:
         print('Epoch: {}/{}.............'.format(epoch, num_epochs), end=' ')
         print("Loss: {:.4f}".format(loss.item()))
-
 
--- rnn_model.py
+++ rnn_model.py
@@ -1,42 +1,57 @@
 import torch
+import numpy as np
 class RNNModel(torch.nn.Module):
     def __init__(self, input_dim, feature_dim, hidden_dim, output_dim, num_layers, batch_size):
         super(RNNModel, self).__init__()
         
+
         # RNN Architecture
         self.hidden_size=hidden_dim
         self.input_size=feature_dim  
         self.batch_size=batch_size
         self.num_layers=num_layers
-        
+        #print("Input dim : ", input_dim," Feature dim: ",feature_dim," hidden dim: ", hidden_dim," output_dim: ",output_dimi)  
         # RNN 
-        self.rnn = torch.nn.RNN(feature_dim, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=True)
+        self.rnn = torch.nn.RNN(input_dim, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=False)
         
         # fc layers
-        self.fc6 = torch.nn.Linear(hidden_dim, 1)
-        self.fc9 = torch.nn.Linear(hidden_dim, 1)
-        self.fc12 = torch.nn.Linear(hidden_dim, 1)
-        self.fc15 = torch.nn.Linear(hidden_dim, 1)    
-        self.linear_list = [self.fc6,self.fc9,self.fc12,self.fc15]
+        self.fc = torch.nn.Linear(hidden_dim, 1)
+        #self.fcFinal = torch.nn.Linear(hidden_dim, 4)
+        self.fc3 = torch.nn.Linear(10, 1)
+        self.fc6 = torch.nn.Linear(3, 1)
+        self.fc9 = torch.nn.Linear(5, 1)
+        self.fc12 = torch.nn.Linear(4, 1)  
+        self.linear_list = [self.fc3,self.fc6,self.fc9,self.fc12]
 
-    def forward(self, inp):
-        output_list = []
+    def forward(self, inp, age3_unique, age6_unique, age9_unique):
+        inp = inp.permute((1,0,2))
         # initialize hidden and cell
         hn = torch.randn(self.num_layers, self.batch_size, self.hidden_size)
-        # cn = torch.nn.init(torch.randn(self.num_layers, self.batch_size, self.hidden_size))
         
         # step through the sequence i.e. for each wave
-        output, hn = self.rnn(inp, hn.float().cuda())
-        # output is [batch size, timestep = 1, hidden dim]
-        # output_list.append(self.linear_list[i](output[:,0,:]))
-        print("Output : ", output.shape)
-        print("Hidden : ", hn.shape)
-        # out = output[:,:,-1]
-        # out = torch.reshape(out[0], (out[0].shape[0],1))
-        print(output.shape)
-        final_out = self.fc6(output[0])
-        print("Final_output : " ,final_out.shape, " final_output : ", final_out[:5])  
-        return final_out, hn
+        output, hn = self.rnn(inp, hn.cuda())
+        final_out_overall = self.fc(output)
+        final_out_overall = final_out_overall.permute((1,0,2))
+        #print("Final Output Shape : ", final_out_overall.shape)
+        final_out_overall = final_out_overall.contiguous().view(final_out_overall.shape[0],-1)
+        #print("Final Output Overall : ", final_out_overall.shape)
+        final_input_age3 = torch.cat((final_out_overall[:,0][:,None],age3_unique),1)
+        final_input_age6 = torch.cat((final_out_overall[:,1][:,None],age6_unique),1)
+        final_input_age9 = torch.cat((final_out_overall[:,2][:,None],age9_unique),1)
+        #final_input_age12 = torch.cat((final_out_overall[:,3][:,None],age12_unique),1)
+        final_out_3 = torch.sigmoid(self.fc3(final_input_age3)).permute(1,0)
+        final_out_6 = torch.sigmoid(self.fc6(final_input_age6)).permute(1,0)
+        final_out_9 = torch.sigmoid(self.fc9(final_input_age9)).permute(1,0)
+        #final_out_12 = torch.sigmoid(self.fc12(final_input_age12)).permute(1,0)
+        #print("Final Output 12 : ", final_out_12)
+        #print(final_out_3.shape, " ", final_out_6.shape, " ")
+        #final_output = torch.cat((final_out_3, final_out_6, final_out_9, final_out_12), dim=0)
+        final_output = torch.cat((final_out_3, final_out_6, final_out_9), dim=0)
+        #final_output = torch.Tensor([final_out_3.cpu().detach().numpy(), final_out_6.cpu().detach().numpy(), final_out_9.cpu().detach().numpy(), final_out_12.cpu().detach().numpy()])
+        #final_out = final_out_6.contiguous().view(final_out_6.shape[0],-1)
+        
+        print("Initial Output: ", output.shape, " Hidden shape: ",hn.shape,"Final_output : " ,final_output.shape)  
+        return final_output, hn
 '''
 	def init_hidden(self, batch_size):
         	# This method generates the first hidden state of zeros which we'll use in the forward pass
